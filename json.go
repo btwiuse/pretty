519ff4ea1882a8b04a22a32fd66c2219911a0e44
navigaid <navigaid@gmail.com>
2019-04-01 15:32:27 +0800

    add {Json,JSON,YAML,Yaml}String method

--- json.go
+++ json.go
@@ -11,10 +11,26 @@ func JSON(v interface{}) {
 	fmt.Printf("%s\n", b)
 }
 
+func JSONString(v interface{}) string {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%s\n", b)
+}
+
 func Json(v interface{}) {
 	b, err := json.Marshal(v)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("%s\n", b)
+}
+
+func JsonString(v interface{}) string {
+	b, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%s\n", b)
 }
